c90072323e5cf18f05591812d7656c89bc477207
Vidyadilesh <98165408+Vidyadilesh@users.noreply.github.com>
2022-01-27 14:53:12 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,28 @@
+package week1.day2;
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		int input=153;
+		//(1*1*1)+(5*5*5)+(3*3*3)=153
+		int orgNum = input;
+				int sum=0;
+				while(input>0)
+				{
+					int rem=input%10;
+					sum=sum+(rem*rem*rem);
+					input=input/10;
+				}
+				if(sum==orgNum)
+				{
+					System.out.println("Armstrong");
+				}else
+					{
+						System.out.println("Not a Armstrong");
+					
+				}
+
+
+	}
+
+}
